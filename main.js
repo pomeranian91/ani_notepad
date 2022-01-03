1780595850004513f548d51acb526f6133333521
pomeranian91 <forking91@gmail.com>
2022-01-03 16:19:42 +0900

    radionav

--- main.js
+++ main.js
@@ -12,3 +12,27 @@ function backcolor(){
 document.querySelector('.btn__changeback--ico').onclick = function(){
 backcolor();
 };
+
+// 전환이 자연스러운 네브바
+let radioglider = document.querySelector('.navradio__glider')
+document.querySelector('.radiobtn1').onclick =function(){
+    radioglider.style.transform = "translate(0,0)"
+    document.querySelector('.radiobtn1').style.color = "red";
+    document.querySelector('.radiobtn2').style.color = "black";
+    document.querySelector('.radiobtn3').style.color = "black";
+};
+
+document.querySelector('.radiobtn2').onclick =function(){
+    radioglider.style.transform = "translate(100%,0)"
+    document.querySelector('.radiobtn1').style.color = "black";
+    document.querySelector('.radiobtn2').style.color = "red";
+    document.querySelector('.radiobtn3').style.color = "black";
+
+};
+
+document.querySelector('.radiobtn3').onclick =function(){
+    radioglider.style.transform = "translate(200%,0)"
+    document.querySelector('.radiobtn1').style.color = "black";
+    document.querySelector('.radiobtn2').style.color = "black";
+    document.querySelector('.radiobtn3').style.color = "red";
+};
